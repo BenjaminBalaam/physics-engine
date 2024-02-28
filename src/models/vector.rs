bba8dc6bd0689a98ff6e96bc326f8866a26f0d7e
CodeSmith00 <15169323+CodeSmith00@users.noreply.github.com>
2024-02-28 20:38:19 +0000

    Implemented basic drawing of objects

--- src/models/vector.rs
+++ src/models/vector.rs
@@ -1,8 +1,20 @@
+use std::ops;
+
+#[derive(Copy, Clone)]
 pub struct Vector {
     pub x: f64,
     pub y: f64,
 }
 
 impl Vector {
-    
+    pub fn new(x: f64, y: f64) -> Vector {
+        Vector {x, y}
+    }
+}
+
+impl ops::AddAssign<Vector> for Vector {
+    fn add_assign(&mut self, rhs: Vector) {
+        self.x += rhs.x;
+        self.y += rhs.y;
+    }
 }
--- src/models/world.rs
+++ src/models/world.rs
@@ -6,37 +6,42 @@ extern crate piston;
 use opengl_graphics::GlGraphics;
 use piston::input::{RenderArgs, UpdateArgs};
 
+use super::Vector;
 use super::Object;
 
 pub struct World {
-    pub gl: GlGraphics, // OpenGL drawing backend
+    pub gl: GlGraphics,
     pub objects: Vec<Object>,
 }
 
 impl World {
+    pub fn new(gl: GlGraphics, objects: Vec<Object>) -> World {
+        World {gl, objects}
+    }
+
     pub fn render(&mut self, args: &RenderArgs) {
         use graphics::*;
 
-        let square = rectangle::square(0.0, 0.0, 50.0);
-
-        for object in self.objects.iter() {
-            self.gl.draw(args.viewport(), |c, gl| {
+        self.gl.draw(args.viewport(), |c, gl| {
+            for object in self.objects.iter() {
                 clear([0.0, 0.0, 0.0, 1.0], gl);
+
+                let square = rectangle::square(-25.0, -25.0, 50.0);
 
                 let transform = c.transform
                     .trans(object.position.x, object.position.y)
                     .rot_rad(object.rotation);
                 
                 rectangle(object.colour, square, transform, gl);
-            });
-        }
+            }
+        });
     }
 
-    pub fn update(&mut self, _args: &UpdateArgs) {
-        // // Rotate 2 radians per second.
-        // self.rotation += 2.0 * args.dt;
+    pub fn update(&mut self, args: &UpdateArgs) {
+        for object in self.objects.iter_mut() {
+            object.update(args);
 
-        // // Fall down over time
-        // self.height -= 0.5;
+            object.velocity += Vector::new(0.0, 0.01);
+        }
     }
 }
